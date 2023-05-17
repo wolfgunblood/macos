ca7776b0e812ce7dc30a86a1d8b64655afe04459
wolfgunblood <jalajdorai1@gmail.com>
2023-05-17 15:29:47 +0530

    Fixed TS Warning

--- src/components/VSCode/VSCode.tsx
+++ src/components/VSCode/VSCode.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import './VSCode.scss';
 import { store } from '../../App';
 import { GrFormClose, GrFormSubtract } from "react-icons/gr";
