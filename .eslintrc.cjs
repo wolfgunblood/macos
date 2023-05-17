34ca163c9b1e7a11e5ba16b47a729638ac9b8a27
wolfgunblood <jalajdorai1@gmail.com>
2023-05-17 15:34:54 +0530

    Added no-var lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,8 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
     "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+
   },
 }
