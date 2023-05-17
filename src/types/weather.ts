4cbcd9fad9601376392c66b38afc55ffbc214004
wolfgunblood <jalajdorai1@gmail.com>
2023-05-17 16:33:00 +0530

    Fixed erros

---
+++ src/types/weather.ts
@@ -0,0 +1,22 @@
+interface weather {
+    name: string,
+    main: {
+        temp: string,
+        humidity: string,
+        feels_like: string,
+    },
+    weather:[
+        {
+            main: string,
+            description: string,
+            icon: string,
+        },
+    ],
+    wind:{
+        speed: string,
+        deg:  string,
+    },
+
+}
+
+export default weather;
